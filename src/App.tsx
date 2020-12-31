3fa570528772402c41f6585627d4817603e8ba3c
zhaoqi <zhaoqi@zhaoqideMacBook-Pro.local>
2020-12-31 14:24:26 +0800

    封装Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Button,{ButtonType,ButtonSize} from './components/Button/button'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button autoFocus>hello</Button>
+        <Button btnType={ButtonType.Default} disabled>hello</Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Large}>hello</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>hello</Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com">hello</Button>
+
+        <h1>hello</h1>
+        <h2>hello</h2>
+        <h3>hello</h3>
+        <code>const a=1</code>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
